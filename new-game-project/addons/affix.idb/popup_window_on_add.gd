df0ad8b558e8f065aaf7e6527b59c4638363bd93
AlwaysEast <lojith@proton.me>
2025-02-05 02:17:01 +0000

    Update popup_window_on_add.gd

--- new-game-project/addons/affix.idb/popup_window_on_add.gd
+++ new-game-project/addons/affix.idb/popup_window_on_add.gd
@@ -7,6 +7,7 @@ var header: TreeItem
 var popup: PopupMenu
 var option_button: OptionButton
 var instanced_ui_elements = []
+var live_id_data = []
 var texture: Texture2D = preload("res://addons/affix.idb/btn_new.png")
 signal button_pressed
 	
@@ -27,67 +28,84 @@ func _on_close_requested() -> void:
 	queue_free()
 	
 func _on_option_button_item_selected(index: int) -> void:
+	# clear the tree
 	tree.clear()
+
+	# get the table name
 	var table_name: String = option_button.get_item_text(index)
-	root = tree.create_item()
 	
-	header = tree.create_item(root)
-	header.set_text(0, table_name)
-	header.set_icon(0, texture)
-	header.set_text_alignment(0, HORIZONTAL_ALIGNMENT_LEFT)
-	header.set_custom_bg_color(0, Color(0.25, 0.26, 0.298))
-	header.set_custom_bg_color(1, Color(0.25, 0.26, 0.298))
-	
+	# create the table tree item
+	var table_tree_item = create_table_tree_item(table_name)
+
+	# get field names belonging to that table
 	Pluggy.database.query("PRAGMA table_info("+table_name+")")
-	var table_fields = Pluggy.database.query_result
-	
-	Pluggy.database.query("PRAGMA foreign_key_list("+table_name+")")
-	var table_foreign_keys = Pluggy.database.query_result
-	
-	for row in table_fields:
+	var table_field_names = Pluggy.database.query_result
+
+	# create an empty row of fields belonging to the `table_tree_item`
+	for row in table_field_names:
+		# because we're adding we don't include id
 		if row["name"] == "id":
 			continue
-		var treeItem: TreeItem = tree.create_item(header)
+		var treeItem: TreeItem = tree.create_item(table_tree_item)
 		treeItem.set_text(0, row["name"])
 		if row["name"].ends_with("id"):
 			treeItem.set_metadata(1, {"table_name":table_name, "name":row["name"]})
 		else:
 			treeItem.set_editable(1, true)
 		treeItem.set_custom_bg_color(1, Color(0.21, 0.21, 0.3))
+
+func create_table_tree_item() -> TreeItem:
+	root = tree.create_item()
+	header = tree.create_item(root)
+	header.set_text(0, table_name)
+	header.set_icon(0, texture)
+	header.set_text_alignment(0, HORIZONTAL_ALIGNMENT_LEFT)
+	header.set_custom_bg_color(0, Color(0.25, 0.26, 0.298))
+	header.set_custom_bg_color(1, Color(0.25, 0.26, 0.298))
+	return header
 	
+# if you select a tree item in column 1 that does not have metadata
 func _on_tree_cell_selected() -> void:
 	var item: TreeItem = tree.get_selected()
+
+	# to ensure we're selecting an applicable row, check to ensure it has metadata
 	if item.get_metadata(1) == null:
 		return
 	popup.clear(true)
+
+	# get the table name and field name from the selected row
 	var table_name = item.get_metadata(1)["table_name"]
 	var row_name = item.get_metadata(1)["name"]
+
+	# get all the foreign keys belonging to the table.
+	# EXAMPLE `{"name":"ItemEntries", "from":"Items", "to":"id"}` 
 	Pluggy.database.query("PRAGMA foreign_key_list(" + table_name + ")")
 	var referenced_table = ""
+	# iterate through foreign keys to find the one relevant to the selected cell and store its table name
 	for row in Pluggy.database.query_result:
-		if row["from"] == row_name:  # Match the foreign key column
-			referenced_table = row["table"]
+		if row["from"] == row_name:
+			referenced_table = row["table"] #<- should this be 'name' ?
 			break
 	
 	if referenced_table != "":
-		Pluggy.database.query("SELECT name FROM " + referenced_table)
+		Pluggy.database.query("SELECT * FROM " + referenced_table)
+		live_id_data.clear()
 		for result in Pluggy.database.query_result:
 			popup.add_item(result["name"])
+			live_id_data.push_back(result["id"])
 			
 	var button_rect: Rect2i = tree.get_item_area_rect(item, 1)
 	var tree_rect: Rect2i = $background/Panel.get_rect()
 	popup.position = self.position + button_rect.position + tree_rect.position
 	popup.popup()
 	
-func _on_popup_menu_id_pressed(id: int) -> void:
-	var selected_item = popup.get_item_text(id)
-	
+func _on_popup_menu_id_pressed(id: int) -> void:	
 	var selected_tree_item = tree.get_selected()
 	if selected_tree_item:
-		selected_tree_item.set_text(1, selected_item)
+		selected_tree_item.set_text(1, popup.get_item_text(id)) 
+		selected_tree_item.set_metadata(1, live_id_data[id])
 	
 func _on_button_pressed() -> void:
-	
 	var table_name = header.get_text(0)
 	var child_count = header.get_child_count()
 
@@ -95,7 +113,9 @@ func _on_button_pressed() -> void:
 	for i in range(0, child_count):
 		var item: TreeItem = header.get_child(i)
 		var key = item.get_text(0)
-		var value = item.get_text(1)
+
+		# Retrieve ID if metadata exists, otherwise use the text value
+		var value = item.get_metadata(1) if item.get_metadata(1) != null else item.get_text(1)
 		row_data[key] = value
 	
 	Pluggy.database.insert_row(table_name, row_data)
