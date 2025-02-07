62658ce20ae652a9b122fb9e10e4f3be95549d32
AlwaysEast <lojith@proton.me>
2025-02-07 02:30:43 +0000

    Update affix.idb.gd

--- new-game-project/addons/affix.idb/affix.idb.gd
+++ new-game-project/addons/affix.idb/affix.idb.gd
@@ -1,16 +1,27 @@
 @tool
 class_name Pluggy extends EditorPlugin
 
-var dock
+var idb_dock
+var imt_dock
 var controller: EditorController
 static var database: SQLite
 
 func _enter_tree():
-	add_custom_type("EditorControl", "TabContainer", preload("res://addons/affix.idb/EditorControl.gd"), preload("res://icon.svg"))
-	dock  = preload("res://addons/affix.idb/editor_test.tscn").instantiate()
-	add_control_to_dock(EditorPlugin.DOCK_SLOT_RIGHT_UL, dock)
+	#add_custom_type("EditorControl", "TabContainer", preload("res://addons/affix.idb/EditorControl.gd"), preload("res://icon.svg"))
+	#add_custom_type("MapEditor", "Control",  preload("res://addons/affix.idb/MapEditor.gd"), preload("res://icon.svg"))
+
+	idb_dock  = preload("res://addons/affix.idb/editor_idb.tscn").instantiate()
+	imt_dock = preload("res://addons/affix.idb/editor_imt.tscn").instantiate()
+
+	add_control_to_dock(EditorPlugin.DOCK_SLOT_RIGHT_UL, idb_dock)
+	add_control_to_dock(EditorPlugin.DOCK_SLOT_RIGHT_UL, imt_dock)
 
 func _exit_tree():
-	remove_custom_type("EditorControl")
-	remove_control_from_docks(dock)
-	dock.free()
+	#remove_custom_type("EditorControl")
+	#remove_custom_type("MapEditor")
+
+	remove_control_from_docks(idb_dock)
+	remove_control_from_docks(imt_dock)
+
+	idb_dock.free()
+	imt_dock.free()
